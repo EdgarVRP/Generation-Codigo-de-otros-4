006bffafb4c935f7a9f3c88b2075b0f1529a6446
EdgarVRP <edgarvrp22@gmail.com>
2023-03-28 08:26:32 -0600

    Se corrigen los errores del codigo

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,21 +1,21 @@
-package com.generation;
+import java.util.Scanner;
 
-public class Codigo4 {
+public class Ejercicio4 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
 
-    Scanner s = new Scanner();
-    
     System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
     String j1 = s.nextLine();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    Scanner s2 = new Scanner();
+
+    System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
+    // Scanner s2 = new Scanner();
     String j2 = s.nextLine();
-    
-    if (j1 == j2)) {
+
+    if (j1 == j2) {
       System.out.println("Empate");
     } else {
       int g = 2;
-      switch(j1) {
+      switch (j1) {
         case "piedra":
           if (j2 == "tijeras") {
             g = 1;
@@ -24,8 +24,8 @@ public class Codigo4 {
         case "papel":
           if (j2 == "piedra") {
             g = 1;
-          
-        case "tijera":
+          }
+        case "tijeras":
           if (j2.equals("papel")) {
             g = 1;
           }
@@ -34,6 +34,5 @@ public class Codigo4 {
       }
       System.out.println("Gana el jugador " + g);
     }
-  
-  
+  }
 }
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Generation-Codigo-de-otros-4
- Revision de codigo de otros generation 
+
+ Revision de codigo de otros generation
+
+Se revisa el codigo y se encuentra que es un juego de piedra, papel o tijera.
+
+No se tiene el metodo main
+
+En el if de la linea 14 se tiene un cierre de parentesis extra
+
+En el if de la linea 25 no tiene cierre de llaves
+
+En la linea de instancia del scanner falta pasar el System.in
+
+El nombre de la clase no es la misma a la del archivo
+
+En la linea 9 tiene otra instancia de scanner no necesaria
+
+Hace falta el import de java.util.Scanner
+
+La igualdad en la linea 28 debe ser "tijeras"
